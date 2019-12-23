70cf5cee94f4ccc18ab6e1d9c2a4683afef9558c
George-Lewis <georgejeffreylewis@gmail.com>
2019-12-23 12:25:27 -0500

    Change iframe themeing to be much less jank - The CSS link is now already in each html file - The iframe style is changed once the iframe is actually ready

--- _js/dark.js
+++ _js/dark.js
@@ -1,18 +1,28 @@
 // Runs when the page is loaded
 function onload() {
 
+    theme = localStorage.getItem("mode")
+
     // Toggle into dark mode if local storage is set to dark
-    if (localStorage.getItem("mode") === "dark") {
-        toggle_mode()
+    if (theme === "dark") {
+        toggle_mode(true)
     } else { // There is no stored value or its "light" (or something else)
         localStorage.setItem("mode", "light")
+        $("#frame").css("visibility", "visible")
     }
 
     var frame = $("#frame")
 
     // If frame exists
     if (frame.length > 0) {
-        frame.ready(init_iframe) // Init the frame
+        if (theme === "dark") {
+            frame.on("load", function() {
+                toggle_iframe()
+                frame.css("visibility", "visible")
+            })
+        } else {
+            frame.css("visibility", "visible")
+        }
     } else {
         console.log("iframe not present")
     }
@@ -21,44 +31,10 @@ function onload() {
 
 $(document).ready(onload)
 
-function check_iframe() {
-
-    var links = $("#frame").contents().find("head link")
-
-    for (i = 0; i < links.length; i++) {
-        if (links[i].href.endsWith("dark.css")) {
-            return true
-        }
-    }
-
-    return false
-
- }
-
-// The iframe'd content doesn't inherit CSS from the outer context
-// So we must add the stylesheet into the iframe manually
-function init_iframe() {
-    console.log("Init iframe")
-    var frame = $("#frame").contents()
-    var count = 0
-    while (!check_iframe()) {
-        frame.find("head").append("<link rel=\"stylesheet\" href=\"/css/dark.css\">")
-        count++
-        if (count > 3) {
-            // For some reason this line can randomly and repeatedly fail
-            // So we try again in 10ms, again and again, until it works
-            console.log("iframe css insertion failed, trying again in 10ms")
-            setTimeout(init_iframe, 10)
-            return
-        }
-    }
-    if (localStorage.getItem("mode") === "dark") {
-        toggle_iframe()
-    }
-}
-
 // Toggles all the classes necessary to make the iframe dark-theme
 function toggle_iframe() {
+
+    console.log("Toggle iframe")
 
     var frame = $("#frame").contents()
 
@@ -68,13 +44,21 @@ function toggle_iframe() {
 
         var body = frame.find("body")
 
+        console.log(html.attr("class"))
+
         html.toggleClass("iframe-body-dark")
+
+        console.log(html.attr("class"))
 
         body.toggleClass("iframe-body-dark")
 
         body.find(".md-fences").toggleClass("md-fences-dark")
 
+        console.log(body.find("code").attr("class"))
+
         body.find("code").toggleClass("code-dark")
+
+        console.log(body.find("code").attr("class"))
 
         body.find("blockquote").toggleClass("blockquote-dark")
 
@@ -83,13 +67,17 @@ function toggle_iframe() {
 }
 
 // Toggles dark theme
-function toggle_mode() {
+function toggle_mode(initial = false) {
 
     var dl = $("#darklight")
 
     var text = dl.text().toLowerCase()
 
-    toggle_iframe()
+    if (!initial) {
+        if ($("#frame").length > 0) {
+            toggle_iframe()
+        }
+    }
 
     $("body").toggleClass("body-dark")
 
