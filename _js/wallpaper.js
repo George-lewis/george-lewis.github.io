90b816190e1fda57122c0fc09b21dc24b16e610d
George-Lewis <georgejeffreylewis@gmail.com>
2019-12-23 12:33:22 -0500

    Add new wallpapers

--- _js/wallpaper.js
+++ _js/wallpaper.js
@@ -1,7 +1,7 @@
 // These values represent how many different wallpapers are available
 // For the dark (dpapers) and light (lpapers) themes
-dpapers = 10
-lpapers = 1
+dpapers = 11
+lpapers = 6
 
 // Runs on page load
 function onload() {
